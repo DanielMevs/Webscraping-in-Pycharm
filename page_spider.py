8db227dd7f0ea18ce3052115cf7b1d75cd6d878a
dannymevs@hotmail.com <dannymevs@hotmail.com>
2021-06-08 19:42:57 -0400

    added code to page spider

--- page_spider.py
+++ page_spider.py
@@ -1,9 +1,18 @@
 import os
 import argparse
+from utilities import url_utilities
 
 def main(database: str, url_list_file: str):
+    big_word_list = []
     print("we are going to work with " + database)
     print("we are going to scan" + url_list_file)
+    urls = url_utilities.load_urls_from_file(url_list_file)
+    for url in urls:
+        print("reading " + url)
+        page_content = url_utilities.load_page(url=url)
+        words = url_utilities.scrape_page(page_contents=page_content)
+        big_word_list.extend(words)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,10 +1,10 @@
 from urllib.request import urlopen
-
+from bs4 import BeautifulSoup
 
 def load_urls_from_file(file_path: str):
     try:
         with open(file_path) as f:
-            content = f.readline()
+            content = f.readlines()
             return content
     except FileNotFoundError:
         print("the file " + file_path + " could not be found");
