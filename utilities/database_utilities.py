6e92c9cf2da778f91c321a33190344631a9fb8f6
dannymevs@hotmail.com <dannymevs@hotmail.com>
2021-06-07 21:47:33 -0400

    added url_utils scraping method

---
+++ utilities/database_utilities.py
@@ -0,0 +1,8 @@
+def create_database(database_path: str):
+    # TODO: generate the database
+    pass
+
+
+def save_words_to_database(database_path: str, word_list: list):
+    # TODO: save the words to the database
+    pass
