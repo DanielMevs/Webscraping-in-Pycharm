de851625c930e49ad2dff3b4982d5adcfe05e19a
dannymevs@hotmail.com <dannymevs@hotmail.com>
2021-06-13 18:06:50 -0400

    added database calls and everything works

--- page_spider.py
+++ page_spider.py
@@ -1,6 +1,6 @@
 import os
 import argparse
-from utilities import url_utilities
+from utilities import url_utilities, database_utilities
 
 def main(database: str, url_list_file: str):
     big_word_list = []
@@ -12,6 +12,12 @@ def main(database: str, url_list_file: str):
         page_content = url_utilities.load_page(url=url)
         words = url_utilities.scrape_page(page_contents=page_content)
         big_word_list.extend(words)
+
+    #database code
+    os.chdir(os.path.dirname(__file__))
+    path = os.path.join(os.getcwd(), "words.db")
+    database_utilities.create_database(database_path=path)
+    database_utilities.save_words_to_database(database_path=path, words_list=big_word_list)
 
 
 if __name__ == "__main__":
--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,8 +1,29 @@
+import sqlite3 as lite
+
 def create_database(database_path: str):
-    # TODO: generate the database
-    pass
+    conn = lite.connect(database_path)
+    with conn:
+        cur = conn.cursor()
+        cur.execute("drop table if exists words")
+        ddl = "CREATE TABLE words (word TEXT PRIMARY KEY NOT NULL,usage_count INT DEFAULT 1 NOT NULL);"
+        cur.execute(ddl)
+        ddl = "CREATE UNIQUE INDEX words_word_uindex ON words (word)"
+        cur.execute(ddl)
+    conn.close()
 
+def save_words_to_database(database_path: str, words_list: list):
+    conn = lite.connect(database_path)
+    with conn:
+        cur = conn.cursor()
+        for word in words_list:
+            #check to see if the word is in there
+            sql = "select count(word) from words where  word = '" + word + "'"
+            cur.execute(sql)
+            count = cur.fetchone()[0]
+            if count > 0:
+                sql = "update words set usage_count = usage_count + 1 where word = '" + word + "'"
+            else:
+                sql = "insert into words(word) values ('" + word + "')"
+            cur.execute(sql)
 
-def save_words_to_database(database_path: str, word_list: list):
-    # TODO: save the words to the database
-    pass
+        print("Database save complete!")
